046143e5ae013d5ef321dd4226e6c4d33aeb468e
Tatiana Arbelaez <tatianarbelaezparra@gmail.com>
2022-09-04 22:29:07 -0500

    Uploading old projects

---
+++ Nivel3_repitiendo_acciones_ejercicios.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 20:00:10 2021
+
+@author: user
+"""
+
+"""
+1. Escriba una función que le sirva para reconocer si una palabra es o no palíndrome
+(se lee igual al derecho y al revés). Su función sólo debe utilizar slices.
+"""
+
+"""
+2. Escriba una función que le permita reconocer si una frase es palíndrome (se deben 
+ignorar los espacios, signos de puntuación y mayúsculas o minúsculas). Por ejemplo, 
+“Anita lava la tina” es una frase palíndrome. Ayuda: el módulo string declara el
+valor ascii_letters que podría serle de utilidad. Eliminar los caracteres que no
+le sirvan probablemente requerirá de un ciclo.
+"""
+
+"""
+3. Escriba una función que utilice slices para extraer las palabras que están en las
+posiciones impares de una frase. Por ejemplo, si se aplicara la función a la
+primera frase de este enunciado el resultado debería ser: ['Escriba', 'función', 
+'utilice', 'para', 'las', 'que', 'en', 'posiciones', 'de', 'frase'].
+"""
+
+"""
+4. Escriba una función que extraiga la primera palabra de una frase (hasta el primer 
+espacio).
+"""
+
+"""
+5. Escriba una función que extraiga la última frase de un párrafo (desde el último
+                                                                punto).
+"""
+
+"""
+6. Escriba una función que calcule los números primos hasta un cierto límite utilizando
+el algoritmo conocido como la Criba de Eratóstenes. El principio fundamental de
+este algoritmo es que una vez se sabe que un número es primo, ninguno de los
+múltiplos de ese número va a ser un número primo así que no es necesario
+verificar nada sobre ellos. La modificación de listas con slices es particularmente 
+útil para solucionar este problema. Ayuda: Recuerde que las listas se pueden
+construir usando repeticiones (ej: [False]*50).
+
+"""
